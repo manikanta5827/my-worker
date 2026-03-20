9f15d9631ba03baaf22e5512abd2dd2d380bdd40
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-03-20 16:17:51 +0530

    updated db env

---
+++ drizzle/migrations/0000_lying_richard_fisk.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `posts` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`title` text NOT NULL,
+	`body` text,
+	`published` integer DEFAULT false NOT NULL,
+	`created_at` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE INDEX `idx_posts_user_id` ON `posts` (`user_id`);--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`created_at` text NOT NULL,
+	`updated_at` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE INDEX `idx_users_email` ON `users` (`email`);
